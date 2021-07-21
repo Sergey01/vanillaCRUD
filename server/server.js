b8ff304eb5b3d39a2c1e64eed3029648026d8eae
Sergey01 <Sergey01@users.noreply.github.com>
2021-07-21 17:14:04 -0400

    Workinhg on front end main.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello')
+    res.status(200).sendFile('../site/index.html');
+});
+
+app.static('main.js', (req, res) => {
+    res.status(200).sendFile('../site/main.js');
+});
+
+app.static('styles.css', (req, res) => {
+    res.status(200).sendFile('../site/styles.css');
+});
+
+app.use( (req, res) => {
+    res.status(404).send('Page Not Found');
 });
 
 app.listen(3000, ()=> console.log('Listening on 3000'));
---
+++ site/main.js
@@ -0,0 +1,21 @@
+const tasksArr = [
+{id: 0,
+text: 'Complete CRUD app.',
+completed: false
+},
+{id:1,
+text: 'Add login functionality.',
+completed: false
+}
+];
+
+const populateTasks = function() {
+    tasksArr.forEach(task => {
+        const taskElem = createElement('div');
+        taskElem.innerText = task.text;
+        taskElem.id = 'task'+task.id.toString();
+        taskElem.complete = task.completed;
+        document.getElementById('tasks').append(taskElem);
+};
+
+populateTasks();
