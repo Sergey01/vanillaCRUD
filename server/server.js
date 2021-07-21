68d632d1d70cf8c0c624a64c0f6a334613a5374f
Sergey01 <Sergey01@users.noreply.github.com>
2021-07-21 14:47:16 -0400

    Basic express functionality added as placeholder

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello')
+});
+
+app.listen(3000, ()=> console.log('Listening on 3000'));
