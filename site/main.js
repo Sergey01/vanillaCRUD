5c2b92d707a4812e65c03d29cde4a44c4f4b8756
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-07-21 22:23:33 -0400

    Added Delete and Complete buttons along with Undo functionality for both of them. So far this is all done front end without talking to the database. There is also no way to create new tasks. Furthermore, the functionality currently resides in index.html and main.js. This may not work (or at least not cleanly) if I implement a login page as the first visible page. I need a login page, a main (app) page, and a sign up page.

--- site/main.js
+++ site/main.js
@@ -9,19 +9,62 @@ completed: false
 }
 ];
 
+// undo delete functionality
+const deletedStack = [];
+
+const undoButton = document.getElementById('undo-delete');
+
+if (deletedStack.length === 0) undoButton.disabled = true;
+
+undoButton.addEventListener('click', (e) => {
+    const deletedTask = deletedStack.pop();
+    deletedTask.style.display = 'revert';
+    if (deletedStack.length === 0) undoButton.disabled = true;
+})
+
+
 const populateTasks = function() {
     tasksArr.forEach(task => {
+        // create task div
         const taskElem = document.createElement('div');
+        
+        // set task text and attributes
         taskElem.innerText = task.text;
         taskElem.id = 'task'+task.id.toString();
         taskElem.className = 'task';
-        taskElem.complete = task.completed;
+        taskElem.dataset.complete = task.completed;
 
-        // add event listeners
+        // add event listeners to task div
         taskElem.addEventListener('mouseover', () => taskElem.style.fontWeight = 'bold');
-        taskElem.addEventListener('mouseout', e => taskElem.style.fontWeight = 'normal');
+        taskElem.addEventListener('mouseout', () => taskElem.style.fontWeight = 'normal');
 
+        // create complete and delete buttons
+        const completeButton = document.createElement('button');
+        completeButton.innerText = 'Complete';
+        const deleteButton = document.createElement('button');
+        
+        deleteButton.innerText = 'Delete';
+        completeButton.addEventListener('click', (e) => {
+            if (!(taskElem.style.textDecoration === 'line-through red')) {
+                taskElem.style.textDecoration = 'line-through red';
+                completeButton.innerText = 'Undo Complete';
+            } else {
+                taskElem.style.textDecoration = 'none';
+                completeButton.innerText = 'Complete';
+            }
+        });
+        deleteButton.addEventListener('click', (e) => {
+            // taskElem.remove();
+            taskElem.style.display = 'none';
+            deletedStack.push(taskElem);
+            undoButton.disabled = false;
+        });
+
+        // attach task div to parent (tasks) div
         document.getElementById('tasks').append(taskElem);
+
+        // attach buttons to task
+        taskElem.append(completeButton, deleteButton);
 })};
 
 populateTasks();
