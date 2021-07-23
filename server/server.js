7ed2d5b4c18d17965e505fa6ec4e05d16a1f7497
Sergey01 <Sergey01@users.noreply.github.com>
2021-07-22 20:09:39 -0400

    Backing up work in progress.

--- server/server.js
+++ server/server.js
@@ -6,14 +6,17 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../site/index.html'))
+    res.status(200)
+    .sendFile(path.join(__dirname, '../site/index.html'), {}, (err) => if (err) {return next(err)})
 });
 
-app.get('main.js', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../site/main.js'))
+app.get('/main.js', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../site/main.js'), {}, (err)=> if (err) return next(err))
 });
 
-app.get('styles.css', (req, res) => {
+app.get('/styles.css', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../site/styles.css'))
 });
 
+
+app.listen(PORT, ()=>console.log(`Listening on port: ${PORT}`));
