b01da03ba283023a606a330df1f58d8a2973ced4
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-07-22 20:19:57 -0400

    Fixed nodemon not loading site error. server.js needed to encapsulate the error handlers in {}.

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ const PORT = 8080;
 
 app.get('/', (req, res) => {
     res.status(200)
-    .sendFile(path.join(__dirname, '../site/index.html'), {}, (err) => if (err) {return next(err)})
+    .sendFile(path.join(__dirname, '../site/index.html'), {}, (err) => {if (err) {return next(err)}})
 });
 
 app.get('/main.js', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../site/main.js'), {}, (err)=> if (err) return next(err))
+  res.status(200).sendFile(path.join(__dirname, '../site/main.js'), {}, (err)=> {if (err) return next(err)})
 });
 
 app.get('/styles.css', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../site/styles.css'))
+  res.status(200).sendFile(path.join(__dirname, '../site/styles.css'), {}, (err)=> {if (err) return next(err)})
 });
 
 
